d3da8ba259b37727bfda06efa13a5b58b7e02a04
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2024-05-11 19:57:29 +0100

    add new homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  text = 'Default starting text';
+  
+  onChangeText() {
+    this.text = 'Changed!';
+  }
 }
